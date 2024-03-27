5aaed7583bab809976d5be7a4f08cc6d584f439c
Archit <architojha420@gmail.com>
2024-03-27 12:26:21 +0530

    integrated login

--- frontend/not-by-bot/src/app/components/auth/login.tsx
+++ frontend/not-by-bot/src/app/components/auth/login.tsx
@@ -2,13 +2,68 @@
 
 import React, { useState } from "react";
 
-import { Input, Button } from "@nextui-org/react";
+import { Input, Button, Spinner } from "@nextui-org/react";
+import supabase from "../../../../supabase";
+import { useRouter } from "next/navigation";
 
 const login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const validateEmail = (value: string) =>
+    value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+
+  const isInvalid = React.useMemo(() => {
+    if (email === "") return false;
+
+    return validateEmail(email) ? false : true;
+  }, [email]);
+
+  const handleSubmitButton = () => {
+    setLoading(true);
+    if (email === "" || password === "") {
+      alert("Please fill all the fields");
+      setLoading(false);
+    } else if (isInvalid) {
+      alert("Enter a Valid Email");
+      setLoading(false);
+    } else {
+      console.log("logging in");
+      loginInUser();
+      setLoading(false);
+    }
+  };
+
+  const loginInUser = async () => {
+    setLoading(true);
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+
+      if (data) {
+        console.log(data);
+        router.push("/marketplace");
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div style={{ display: "flex", flexDirection: "column", flex: 1, justifyContent: 'center' }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
       <h1 className="text-3xl font-bold text-black">Login User</h1>
 
       <div
@@ -20,12 +75,15 @@ const login = () => {
         }}
       >
         <Input
-          size="sm"
-          type="email"
-          label="Email Address"
-          style={{ width: "100%" }}
           value={email}
+          type="email"
+          label="Email"
+          variant="bordered"
+          isInvalid={isInvalid}
+          color={isInvalid ? "danger" : "success"}
+          errorMessage={isInvalid && "Please enter a valid email"}
           onValueChange={setEmail}
+          className=" text-black"
         />
       </div>
 
@@ -54,9 +112,18 @@ const login = () => {
           marginTop: 20,
         }}
       >
-        <Button variant="ghost" color="primary" size="sm">
-          Login
-        </Button>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Button
+            variant="ghost"
+            color="default"
+            size="sm"
+            onPress={() => handleSubmitButton()}
+          >
+            Login
+          </Button>
+        )}
       </div>
     </div>
   );
